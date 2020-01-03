f9c75baacdcb75c6005cd9e989d266188fe2bf5e
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-01-03 15:19:58 -0500

    Update scripts.js

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -49,59 +49,64 @@ fetchData('https://randomuser.me/api/?results=12&inc=picture,name,email,location
 function generateCardHTML(data) {
     const html = data.map(user => 
         `<div class="card">
-    <div class="card-img-container">
-        <img class="card-img" src="${user.picture.medium}" alt="profile picture">
-    </div>
-    <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
-        <p class="card-text">${user.email}</p>
-        <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
-    </div>
-    </div>
-`).join(''); 
-    
-userList.innerHTML += html;
+            <div class="card-img-container">
+                <img class="card-img" src="${user.picture.medium}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="card-text">${user.email}</p>
+                <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+            </div>
+            </div>
+        `
+        ).join(''); 
+        // Grabs the previously defined variable userList and adds to it the innerHTML content of html to the page. 
+        userList.innerHTML += html;
 
+    // Creates a variable to grab all elements with the class of card that was added to the DOM. 
     let cardItems = document.querySelectorAll('.card');
-    let items = [...cardItems]
-    console.log(items);
-    items.map(item => {
-            // Create event listener onto each card, so that a modal will popuplate. 
-                item.addEventListener('click', (e) => {
-                    data.map(user => {
-                        // The Logic states that, when a user is click and the innerhtml content is equal to the user first name, create and show that users info onto page.
-                        if(e.target.innerHTML.indexOf(user.name.first) > -1) {
-                            // Creates HTML layout for each user that is clicked. 
-                             let modalDisplay = `
-                                    <div class="modal-container">
-                                    <div class="modal">
-                                        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                                        <div class="modal-info-container">
-                                            <img class="modal-img" src="${user.picture.large} " alt="profile picture">
-                                            <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
-                                            <p class="modal-text">${user.email}</p>
-                                            <p class="modal-text cap">${user.location.city}</p>
-                                            <hr>
-                                            <p class="modal-text">${user.phone}</p>
-                                            <p class="modal-text">${user.location.street.number}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
-                                            <p class="modal-text">Birthday: ${user.dob.date}</p>
-                                        </div>
-                                    </div>
-                                    `;
-                            // Appends the innerHTML to the newly created Div Const variable defined at the beginning
-                            modalDiv.innerHTML = modalDisplay;
-                            bodyElement.appendChild(modalDiv);       
-                        }   
-                    });
 
-                    // This assigns a click event to the modal window 
-                    const closeModal = document.getElementById('modal-close-btn');
-                                closeModal.addEventListener('click', () => {
-                                    // Removes the added Div element from the HTML once clicked. 
-                                    bodyElement.removeChild(modalDiv);
-                                });
-                })
-     });
+    // Uses the spread operator to create a new array of html elements
+    let items = [...cardItems];
+
+        // Maps through each collected item, adds an event listener to each card item. 
+        items.map(item => {
+                // Create event listener onto each card, so that a modal will popuplate. 
+                    item.addEventListener('click', (e) => {
+                        data.map(user => {
+                            // The Logic states that, when a user is click and the innerhtml content is equal to the user first name, create and show that users info onto page.
+                            if(e.target.innerHTML.indexOf(user.name.first) > -1) {
+                                // Creates HTML layout for each user that is clicked. 
+                                let modalDisplay = `
+                                        <div class="modal-container">
+                                        <div class="modal">
+                                            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                                            <div class="modal-info-container">
+                                                <img class="modal-img" src="${user.picture.large} " alt="profile picture">
+                                                <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                                                <p class="modal-text">${user.email}</p>
+                                                <p class="modal-text cap">${user.location.city}</p>
+                                                <hr>
+                                                <p class="modal-text">${user.phone}</p>
+                                                <p class="modal-text">${user.location.street.number}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
+                                                <p class="modal-text">Birthday: ${user.dob.date}</p>
+                                            </div>
+                                        </div>
+                                        `;
+                                // Appends the innerHTML to the newly created Div Const variable defined at the beginning
+                                modalDiv.innerHTML = modalDisplay;
+                                bodyElement.appendChild(modalDiv);       
+                            }   
+                        });
+
+                        // This assigns a click event to the modal window 
+                        const closeModal = document.getElementById('modal-close-btn');
+                                    closeModal.addEventListener('click', () => {
+                                        // Removes the added Div element from the HTML once clicked. 
+                                        bodyElement.removeChild(modalDiv);
+                                    });
+                    });
+        });
 }
 
 
